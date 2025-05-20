fc289de5c051d8cc37f10affb28d15d050607864
Paul Garcia <paulgarcia@Pauls-MacBook-Pro.local>
2025-05-20 00:03:14 -1000

    bootstrap instead of tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client"
-import App from "./App"
-import './index.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
   const env = loadEnv(mode, process.cwd(), '');
   
   return {
-    plugins: [
-      react(),
-    ],
+    plugins: [react()],
+    css: {
+      devSourcemap: true,
+    },
     server: {
-      port: parseInt(env.VITE_PORT || '5173', 10), // Use VITE_PORT or default to 5173
-      host: '0.0.0.0', // Allow external connections
+      port: parseInt(env.VITE_PORT || '5173', 10),
+      host: '0.0.0.0',
     },
   };
 });
