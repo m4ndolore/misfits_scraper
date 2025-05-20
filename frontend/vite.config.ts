b941a492754a172745547b205f5fcae4b1cedbd1
Paul Garcia <paulgarcia@Pauls-MacBook-Pro.local>
2025-05-19 18:49:39 -1000

    fixed tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  server: {
-    port: parseInt(process.env.VITE_PORT || '5173', 10), // Use VITE_PORT or default to 5173
-    host: '0.0.0.0', // Allow external connections
-  },
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [
+      react(),
+    ],
+    server: {
+      port: parseInt(env.VITE_PORT || '5173', 10), // Use VITE_PORT or default to 5173
+      host: '0.0.0.0', // Allow external connections
+    },
+  };
 });
