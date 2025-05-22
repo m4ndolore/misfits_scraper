fcf700995ba78ad30192dc5e9f3d43486e0a1c39
Paul Garcia <paulgarcia@Pauls-MacBook-Pro.local>
2025-05-21 20:19:12 -1000

    pop-up

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,42 +1,50 @@
 // src/types/index.ts
+export * from './Topic';
+
+export interface TopicManager {
+  name?: string;
+  email?: string;
+  phone?: string;
+}
+
 export interface FilterOption {
-    value: string;
-    label: string;
-  }
-  
-  export interface FilterSchema {
-    components: FilterOption[];
-    programs: FilterOption[];
-    topicStatuses: FilterOption[];
-    modernizationPriorities: FilterOption[];
-    technologyAreas: FilterOption[];
-    solicitations: FilterOption[];
-  }
-  
-  export interface ApiTopicForFetch {
-    topicCode: string;
-    topicId: string;
-    topicTitle?: string;
-    phaseHierarchy?: string;
-    component?: string;
-    program?: string;
-    topicStatus?: string;
-    solicitationTitle?: string;
-    topicManagers?: any[];
-    technologyAreas?: string[];
-    modernizationPriorities?: string[];
-    noOfPublishedQuestions?: number;  
-  }
-  
-  export interface Topic {
-    topicCode: string;
-    topicId: string;
-    topicTitle: string;
-    phaseHierarchy: string;
-    component: string;
-    program: string;
-    topicStatus: string;
-    solicitationTitle: string;
-    topicManagers: any[];
-    numQuestions?: number;
-  }
+  value: string;
+  label: string;
+}
+
+export interface FilterSchema {
+  components: FilterOption[];
+  programs: FilterOption[];
+  topicStatuses: FilterOption[];
+  modernizationPriorities: FilterOption[];
+  technologyAreas: FilterOption[];
+  solicitations: FilterOption[];
+}
+
+export interface ApiTopicForFetch {
+  topicCode: string;
+  topicId: string;
+  topicTitle?: string;
+  phaseHierarchy?: string;
+  component?: string;
+  program?: string;
+  topicStatus?: string;
+  solicitationTitle?: string;
+  topicManagers?: TopicManager[];
+  technologyAreas?: string[];
+  modernizationPriorities?: string[];
+  noOfPublishedQuestions?: number;
+}
+
+export interface Topic {
+  topicCode: string;
+  topicId: string;
+  topicTitle?: string;
+  phaseHierarchy?: string;
+  component?: string;
+  program?: string;
+  topicStatus?: string;
+  solicitationTitle?: string;
+  topicManagers?: TopicManager[];
+  numQuestions?: number;
+}
